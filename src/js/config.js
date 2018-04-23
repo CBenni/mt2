55f086b7eeb9cba82ae481748359c9c87adadf7c
CBenni <levelcarnage@gmail.com>
2018-04-23 12:11:28 +0200

    Fixed production config

--- src/js/config.js
+++ src/js/config.js
@@ -5,7 +5,7 @@ import stagingConfig from './config.staging.json';
 console.log('Using config ', process.env.NODE_ENV);
 
 let config; // eslint-disable-line import/no-mutable-exports
-if (process.env.NODE_ENV === 'production') config = prodConfig;
+if (process.env.NODE_ENV === 'prod') config = prodConfig;
 else if (process.env.NODE_ENV === 'staging') config = stagingConfig;
 else config = devConfig;
 export default config;
